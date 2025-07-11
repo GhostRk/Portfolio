b447946cfa52d6999a2001270069f4c605827e38
GhostRk <64723476+GhostRk@users.noreply.github.com>
2025-07-11 23:19:13 +0530

    Update Hero.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -11,7 +11,7 @@ import Loader from "../components/Loader";
 const Hero = () => {
   const isMobile = useMediaQuery({ maxWidth: 853 });
   return (
-    <section className="flex items-start justify-center min-h-screen overflow-hidden md:items-start md:justify-start c-space">
+    <section id="home" className="flex items-start justify-center min-h-screen overflow-hidden md:items-start md:justify-start c-space">
       <HeroText />
       <ParallaxBackground />
       <figure
@@ -23,7 +23,7 @@ const Hero = () => {
             <Float>
               <Astronaut
                 scale={isMobile && 0.23}
-                position={isMobile && [0, -1.5, 0]}
+                position={isMobile ? [-1, -1.5, 0] : [3, 0, 0]}
               />
             </Float>
             <Rig />
