2f1de114a90d1e9298aa1f4f07909680303fec90
GhostRk <64723476+GhostRk@users.noreply.github.com>
2025-07-11 23:20:46 +0530

    Update Experiences.jsx

--- src/sections/Experiences.jsx
+++ src/sections/Experiences.jsx
@@ -2,9 +2,11 @@ import { Timeline } from "../components/Timeline";
 import { experiences } from "../constants";
 const Experiences = () => {
   return (
+    <section id="experience">
     <div className="w-full">
       <Timeline data={experiences} />
     </div>
+    </section>
   );
 };
 
